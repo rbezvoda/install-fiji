fd088c3f40aa2556dc17f40a4ae80b2e1b7806ba
rbezvoda <39493279+rbezvoda@users.noreply.github.com>
2022-03-08 12:38:36 +0100

    Initial version
    Downloads and unzips Fiji, creates a link file to the Desktop

---
+++ imagej.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module -Name Microsoft.PowerShell.Archive
+$tmp_path = [System.Environment]::ExpandEnvironmentVariables("%temp%")
+$imagej_zip_file = "$tmp_path\imagej.zip"
+Invoke-WebRequest -Uri "https://downloads.imagej.net/fiji/latest/fiji-win64.zip" -OutFile $imagej_zip_file
+Expand-Archive -path $imagej_zip_file -DestinationPath C:\ -Force
+New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop\Fiji.lnk" -Target "C:\Fiji.app\ImageJ-win64.exe"
+Remove-Item $imagej_zip_file
---
+++ run-fiji.bat
@@ -0,0 +1,14 @@
+NET SESSION
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+GOTO ADMINTASKS
+
+:ELEVATE
+CD /d %~dp0
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();"
+EXIT
+
+:ADMINTASKS
+set path1=%cd%
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0\imagej.ps1"
+pause
+EXIT
